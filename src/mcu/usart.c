409e70e177ff2a88958a132833cf984f002508ae
User <loh@alicia.valkyrie.dynalias.org>
2009-02-16 20:19:11 -0600

    Removed wt_32_SPI Loader because it isn't necessary anymore (we can use a universal programmer.
    Started a new file for one of the microcontrollers, designed for serial communications at fast speed.

---
+++ src/mcu/usart.c
@@ -0,0 +1,9 @@
+/*******
+* usart.c - sample code for communication over a serial link.
+*/
+#define USART_BAUDRATE 921600
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16)) - 1)
+
+int main(void) {
+
+}
