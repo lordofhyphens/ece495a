6f4cebba3b036d1903f962d91d6921e122e97bbb
User <loh@alicia.valkyrie.dynalias.org>
2009-02-16 20:57:59 -0600

    Small update to USART file.

--- src/mcu/usart.c
+++ src/mcu/usart.c
@@ -1,9 +1,18 @@
 /*******
-* usart.c - sample code for communication over a serial link.
+* usart.c - simple code for communication over a serial link.
+* Written by Joseph Lenox, derived in part from code available at
+* http://www.nongnu.org/avr-libc/user-manual/group__stdiodemo.html
+*
 */
 #define USART_BAUDRATE 921600
 #define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16)) - 1)
 
-int main(void) {
+#include <avr/io.h>
 
+// Set up the USART interface with our desired baud rate.
+void usart_init(void) {
+	UBRRL = BAUD_PRESCALE;
+
+	// This enables both transmit and recieve.
+	UCSRB = _BV(TXEN) | _BV(RXEN);
 }
