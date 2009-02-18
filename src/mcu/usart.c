3c2ae55cb980a9c7e874972993e679dbd3d71e9a
User <loh@alicia.valkyrie.dynalias.org>
2009-02-18 01:51:56 -0600

    Added a new url reference to Atmel microcontroller programming, as well as some source code for the dedicated controller

--- src/mcu/usart.c
+++ src/mcu/usart.c
@@ -4,15 +4,47 @@
 * http://www.nongnu.org/avr-libc/user-manual/group__stdiodemo.html
 *
 */
-#define USART_BAUDRATE 921600
-#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16)) - 1)
-
 #include <avr/io.h>
+#include <stdio.h>
+#define USART_BAUDRATE 921600
+#define BAUD_PRESCALE ((F_CPU / (USART_BAUDRATE * 16)) - 1)
 
-// Set up the USART interface with our desired baud rate.
-void usart_init(void) {
-	UBRRL = BAUD_PRESCALE;
+void uart_init (void) {
+	UCSRB |= (1 << RXEN) | (1 << TXEN); 
+	// Turn on the transmission and reception circuitry
+	UCSRC |= (1 << URSEL) | (1 << UCSZ0) | (1 << UCSZ1); // Use 8-bit character sizes
 
-	// This enables both transmit and recieve.
-	UCSRB = _BV(TXEN) | _BV(RXEN);
+	UBRRL = BAUD_PRESCALE; // Load lower 8-bits of the baud rate value into the low byte of the UBRR register
+	UBRRH = (BAUD_PRESCALE >> 8); // Load upper 8-bits of the baud rate value into the high byte of the UBRR register
+}
+// initialize the I/O ports we'll be using.
+void io_init(void) {
+}
+int main(void) {
+	// Set up the uart.
+	unsigned char ByteToSend;
+	unsigned char nextByte;
+	unsigned char zero = 0;
+	unsigned int n = 0;
+	uart_init();
+	io_init();
+	//Set up the first two bytes to send.
+	ByteToSend = PORTC;
+	nextByte = ((1 >> PORTD7) & PORTD6);
+	// Loop forever ! 
+	while (1) {
+		// Wait for the buffer to clear before filling it more.
+		while ((UCSRA & (1 << UDRE)) == 0) {}; 
+		UDR = ByteToSend;
+		n++;
+		if (n > 4) { n = 0; }
+		ByteToSend = nextByte |= ((2*n) >> PORTC);
+		nextByte = zero; // clear the buffer between writes
+		if (n!=0) {
+			nextByte = (unsigned int)((2*n) << PORTC) | (2*n) >> PORTD6 | (((2*n)+1) >> PORTD7);
+		} else {
+			nextByte = (1 >> PORTD7) & PORTD6;
+		}
+	}
+	return 0;
 }
