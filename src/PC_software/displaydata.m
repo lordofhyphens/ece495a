2b72fa845f8a85dca0154931e391f0d6f4dcef6c
nhamann <skronkasaurus@gmail.com>
2009-02-25 16:12:28 -0600

    Added the newest copy of acqdata.py and displaydata.m

--- src/PC_software/displaydata.m
+++ src/PC_software/displaydata.m
@@ -1,8 +1,26 @@
-pathToSoft = 'C:\Documents and Settings\Le Snarf\Desktop\pcsoft\';
-filename = input("Please enter data file name: ");
+function [val, count] = displaydata(path)
 
-datafilepath = [pathToSoft, "data\\", filename];
-disp(datafilepath);
+	if (nargin != 1)
+		usage("displaydata(filename)");
+		return;
+	else
+		dfid = fopen(path, "r");
+		[val, count] = fread(dfid, 20, "uint8");
 
-dfid = fopen(datafilepath, "r");
-[val, count] = fread(dfid, 20, "uint8");
+		n = 0:count-1;
+		stem(n, val);
+
+		undloc = findstr(path, "_");
+		fnameloc = rindex(path,"\\");
+
+		fdate = substr(path, fnameloc+1, (undloc)-(fnameloc+1));
+		fnum = substr(path, undloc+1, 1);
+
+		disp(['fdate = ', fdate, '; fnum = ', fnum]);
+		title(['Plot of ', fdate, ' - ', fnum]);
+
+	endif
+
+
+endfunction
+
