95e6c745f2d3ab082c0baf0064bfb4d3a4d16620
unknown <Team74@.ECElab2.engr.siu.edu>
2009-02-25 12:55:37 -0600

    Stub code for main MCU and some library code for the USART.

---
+++ src/mcu/main_control.c
@@ -0,0 +1,8 @@
+/** 
+	main_control.c - datapath control logic.
+**/
+
+#include "main_control.h";
+
+int main(void) {
+}
---
+++ src/mcu/usart.h
@@ -0,0 +1,14 @@
+/** 
+	Prototypes and definitions for code dealing with the management
+	of the USART interface of AVR 8515 microcontrollers.
+**/
+
+void usart_init() {
+}
+
+void usart_close() {
+}
+
+void usart_set_baud_rate() {
+}
+
