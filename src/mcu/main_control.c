8a8b14e215257579674ac0ccafedd0526f960dcf
unknown <Lenox@.(none)>
2009-03-27 00:04:01 -0500

    Updated MCU and serial unit (SU) main control code.

--- src/mcu/main_control.c
+++ src/mcu/main_control.c
@@ -4,7 +4,22 @@
 
 #include "main_control.h"
 #include "control_util.h"
+#include "control_defines.h"
+#define SPI_SLAVE 1
+#include "spi_util.h"
+
+
 int main(void) {
+	char cmd;
+	// Starting initializiations.
+	SPI_Slave_Init();
+
+	// Get the initial configuration from the other controller.	
+	cmd = SPI_Slave_Receive();
+	// loop forever for now, we'll use an interrupt to reset.
+	while(1) {
+		
+	}
 }
 
 
--- src/mcu/usart.c
+++ src/mcu/usart.c
@@ -5,28 +5,41 @@
 *
 */
 #include <avr/io.h>
+#include <avr/interrupts.h>
 #include <stdio.h>
+#define SPI_MASTER 1
 #define USART_BAUDRATE 921600
 #define BAUD_PRESCALE ((F_CPU / (USART_BAUDRATE * 16)) - 1)
 
 // This needs to be after the defines because the utils use them.
 #include "usart_util.h"
+#include "spi_util.h"
 
 int main(void) {
 	// Set up the uart.
 	unsigned char ByteToSend;
 	unsigned char bufferByte;
+	unsigned char command;
 	unsigned char zero = 0;
 	unsigned int n = 0;
 	uart_init();
 	io_init();
+	// set up the SPI.
+	SPI_Master_Init();
 
 	// TODO: Allow us to start and stop the loop, based on a given input port.
 	// Probably will involve some tri-stating of the USART transmit/receive.
 
+	// Wait for the USART recieve buffer to get a command for the device.
+	while (!(UCSRA & (1<<RXC))) {;}
+	command = UDR;
+
+	SPI_Master_Transmit(command, 1);
+
 	//Set up the first two bytes to send.
 	ByteToSend = PORTC;
 	bufferByte = ((1 >> PORTD7) | PORTD6);
+	
 	// Loop forever !
 	while (1) {
 		// Wait for the buffer to clear before filling it more.
@@ -48,3 +61,13 @@ int main(void) {
 	}
 	return 0;
 }
+
+// Recieve a new command for the device.
+ISR(UART_RX_vect) 
+{
+	// Just to be sure, wait to make sure we're good to go.
+	while (!(UCSRA & (1<<RXC))) {;}
+	command = UDR;
+	SPI_Master_Transmit(command, 1);
+}
+
