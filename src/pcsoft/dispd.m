ad91d64eab4508068bb232c5b5d12748e1702563
nhamann <skronkasaurus@gmail.com>
2009-03-31 23:26:54 -0500

    Updated dispd so that it works, for now.

--- src/pcsoft/dispd.m
+++ src/pcsoft/dispd.m
@@ -1,9 +1,5 @@
 function dispd
 	pathto = "data\\";
-	next = "next";
-	n = "next";
-	quit = "quit";
-	q = "quit";
 
 	if(nargin != 0)
 		usage("dispd");
@@ -11,31 +7,34 @@ function dispd
 	else
 		acfid = fopen("acqconfig.txt", "r");
 		txt = fgetl(acfid);
-		semipos = findstr(txt, ";");
-		
-		startfile = substr(txt, 1, semipos(1) - 1);
 
-		if(semipos(2) > semipos(1) + 1)
-			endfile = substr(txt, (semipos(1) + 1), semipos(2) - (semipos(1) + 1));
-		else
-			endfile = "";
-		endif
+		if(txt != -1)
+			colpos = index(txt, ":");
+			pref = substr(txt, 1, colpos-1);
+			startfile = strcat(pref, 'a.dat');
 
-		fclose(acfid);
-		readandplot(startfile, pathto);
+			if(length(txt) > colpos)
+				endfile = strcat(pref, substr(txt, colpos+1, 1), '.dat');
+			else
+				endfile = "";
+			endif
 
-		if(strcmp(endfile, "") != 1)
-			nextfile = getnextfile(startfile);
+			fclose(acfid);
+			readandplot(startfile, pathto);
 
-			do
-				uin = menu("Plot options ", "Display next plot", "Quit");
+			if(strcmp(endfile, "") != 1)
+				nextfile = getnextfile(startfile);
 
-				if(uin == 1)
-					readandplot(nextfile, pathto);
-					nextfile = getnextfile(nextfile);
-				endif
-			until(strcmp(nextfile, endfile) == 1 || uin == 2);
+				do
+					uin = menu("Plot options ", "Display next plot", "Quit");
 
+					if(uin == 1)
+						readandplot(nextfile, pathto);
+						nextfile = getnextfile(nextfile);
+					endif
+				until(strcmp(nextfile, endfile) == 1 || uin == 2);
+
+			endif
 		endif
 	endif
 endfunction
