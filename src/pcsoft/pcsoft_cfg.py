5d7c401f0966d6067c30cd3252a5932b712eb021
Joseph Lenox <lenox.joseph@gmail.com>
2009-04-18 23:44:59 -0500

    Merged the files together.

--- src/pcsoft/pcsoft_cfg.py
+++ src/pcsoft/pcsoft_cfg.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD:src/pcsoft/pcsoft_cfg.py
-global sckhost, sckport, pathto, fsuffix, partsize
-
-sckhost = 'localhost'
-sckport = 19364
-pathtodata = 'data\\'
-fsuffix = '.dat'
-partsize = 100 # In KiB
-
-=======
 global sckhost, sckport, pathto, fsuffix, partsize
 
 sckhost = 'localhost'
@@ -18,4 +8,3 @@ partsize = 100 # In KiB
 acqpgsize = 20
 keeppgonacq = True
 
->>>>>>> d403bb6cf120e42c1b14906095985bf57d65ec4d:src/pcsoft/pcsoft_cfg.py
